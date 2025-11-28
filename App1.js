16b33d4282e64fbd2d91398c1c16ceb88a4f607b
Harshith-2007 <koolharshith@gmail.com>
2025-11-28 14:58:14 +0530

    Create App1.js

---
+++ App1.js
@@ -0,0 +1,31 @@
+ 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/login" element={<Login />} />
+
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
+
+        {/* default route */}
+        <Route path="*" element={<Login />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
